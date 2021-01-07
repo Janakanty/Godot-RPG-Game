8f73533875f091d82e9141111dd4291f20d10fe2
Janakanty <adam.kowalski.pocztaemail@gmail.com>
2021-01-07 09:48:04 +0100

    nowe tresci

---
+++ NPC's/Covon/NPC-Covon.gd
@@ -0,0 +1,80 @@
+extends StaticBody2D
+
+export (Color,RGB) var mouse_out
+export (Color,RGB) var mouse_over
+
+var current_dialogue = ['']
+var dialog1=false
+var dialog2=false
+
+
+var Covon_start = [
+	{
+		'background': "res://art/Sprites_NPC/portraitCOVON.png"
+	},
+	
+	{'text': 'Świetnie, że sie obudziiłeś! Pojawiłeś się znikąd i stałeś tak przez kilka lat. Wzięliśmy Chomika aby cię pilnował'},
+	{'text':'Zeznajom się z tym ... światem. Wiem, że mało pamiętasz ale każdy z nas rozumiał co to znaczy się przejść'},
+	{'text':'przejdź się, porozmawiaj z innymi, zaznajom się co tu się dzieje, a potem przyjdź do mnie'},
+	{'text':'zrozumiałeś?'},	
+]
+
+
+var Covon_click2 =[
+	{
+		'background': "res://art/Sprites_NPC/portraitCOVON.png"
+	},
+	{'text': 'Ruchy rychy! Nie mamy całej wieczności!... A może mamy?'},
+	{
+		'background': "res://art/Sprites/portrait.png"
+	},
+	{'name':'hero','text':'Kim ty jesteś? Kim ja jestem?'},
+	{
+		'background': "res://art/Sprites_NPC/portraitCOVON.png"
+	},
+	{'name':'Covon','text':'Sluchaj, ja nie jestem tu od przypominania Ci kim jesteś. Idź do Samiry ona podpowie Ci jak odzyskać pamięć.'},
+	{'name':'Covon','text':'Ja nazywam się Covon. Ale bez przesady, swoje imie powinieneś pamiętać. Czyż nie?'},
+	{
+	'input': 'Jak masz na imię?',
+	'window_title': 'Wpisz swoje imie',
+	'variable': 'name'
+	},
+	{'name':'Covon','text':'A więc witaj [name] . Tak jak mówiłem. Przejdź się tu trochę i wróć do mnie'},
+	{'name':'Covon','text':'Na pewno będę miał tu jakieś zadanie dla Ciebie'},
+	{
+		'background': "res://art/Sprites/portrait.png"
+	},
+	{'name':'[name]','text':'...'},
+	
+	
+]
+
+
+func _on_Area2D_mouse_entered(): #Zmienia kolor postaci po najechaniu myszką 
+	set_modulate(mouse_over)
+	pass
+
+func _on_Area2D_mouse_exited(): #Zmienia kolor postaci po odjechaniu myszką 
+	set_modulate(mouse_out)
+	pass
+
+func what_dialogue_should_be(): #system wyboru, który dialog powinien teraz się wczytać
+	if dialog1==false:
+		current_dialogue=Covon_start
+		dialog1=true
+	elif dialog2==false:
+		current_dialogue=Covon_click2
+
+		
+func _on_Area2D_input_event(viewport, event, shape_idx):  #wywołanie dialogu po nacisnieciu w NPC'a
+	if (event is InputEventMouseButton && event.button_index == BUTTON_LEFT && event.pressed):
+		what_dialogue_should_be()
+		var dialog = preload("res://addons/dialogs/Dialog.tscn").instance()
+		var camera = get_node("../Player/Camera2D/CanvasLayer")
+		dialog.dialog_script=current_dialogue
+		camera.add_child(dialog)
+			
+			
+		
+		
+	pass # Replace with function body.
